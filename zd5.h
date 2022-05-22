dd230664f38b3e4b087a7a3ea1d00c4ac8e26192
Krav_no0r@student.itstep.org <Krav_no0r@student.itstep.org>
2022-05-22 14:11:00 +0300

    1

--- zd5.h
+++ zd5.h
@@ -1,1 +1,75 @@
-#pragma once
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+void pol(int* dar, int size, int* pol)
+{
+    *pol = 0;
+    for (int i = 0; i < size; i++)
+        if (dar[i] > 0)
+            pol++;
+    cout << *pol << endl;
+}
+void min(int* dar, int size, int* min)
+{
+    *min = 0;
+    for (int i = 0; i < size; i++) {
+        if (dar[i] < 0) {
+            min += 1;
+        }
+    }
+    cout << *min << endl;
+}
+
+void null(int* dar, int size, int* null)
+{
+    *null = 0;
+    for (int i = 0; i < size; i++) {
+        if (dar[i] = 0) {
+            null++;
+        }
+    }
+    cout << *null << endl;
+}
+
+
+
+
+
+void main()
+{
+    srand(time_t(NULL));
+
+    int size;
+    int* dar;
+    int pol1 = 0;
+    int min1 = 0;
+    int null1 = 0;
+
+    cout << "Enter size:\n";
+    cin >> size;
+
+    dar = new int[size];
+    if (!dar) {
+        cout << "Sorry, error!!!";
+        exit(0);
+    }
+
+    for (int i = 0; i < size; i++) {
+        dar[i] = rand() % 41 - 20;
+        cout << dar[i] << "\t";
+    }
+    cout << "\n\n";
+
+    pol(dar, size, &pol1);
+    min(dar, size, &min1);
+    null(dar, size, &null1);
+
+    delete[]dar;
+}
+
+
+
+
